f470cc4bed00bd42ae4dd3c59e1e723c73262bb6
siripongn <siripong.ut.n@gmail.com>
2024-07-05 14:31:49 +0700

    rest commit

--- README.md
+++ README.md
@@ -24,7 +24,69 @@ app.use(bodyParser.urlencoded({
 POST
 localhost:8888/newuser
 
+docker pull mysql:latest
+docker run --name test-mysql -e MYSQL_ROOT_PASSWORD=mysql#pass -p 3306:3306 -d mysql:latest
+docker exec -it test-mysql bash
+mysql -u root -p
+CREATE DATABASE todos;
+CREATE TABLE todolist(id int NOT NULL AUTO_INCREMENT,
+name varchar(50) NOT NULL, 
+status varchar(50), 
+date_created DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, 
+PRIMARY KEY (id));
+
+CREATE TABLE quote
+( id INT(11) NOT NULL AUTO_INCREMENT , 
+ quote VARCHAR(255) NOT NULL , 
+ author VARCHAR(255) NOT NULL , 
+ created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+ updated_at DATETIME on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+ PRIMARY KEY (id), 
+ INDEX idx_author (author), UNIQUE idx_quote_uniqie (quote)
+) 
+ ENGINE = InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_general_ci;
+
+INSERT INTO quote (id, quote, author) VALUES
+(1, 'There are only two kinds of languages: the ones people complain about and the ones nobody uses.', 'Bjarne Stroustrup'),
+(3, 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', 'Martin Fowler'),
+(4, 'First, solve the problem. Then, write the code.', 'John Johnson'),
+(5, 'Java is to JavaScript what car is to Carpet.', 'Chris Heilmann'),
+(6, 'Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.', 'John Woods'),
+(7, "I'm not a great programmer; I'm just a good programmer with great habits.", 'Kent Beck'),
+(8, 'Truth can only be found in one place: the code.', 'Robert C. Martin'),
+(9, "If you have to spend effort looking at a fragment of code and figuring out what it's doing, then you should extract it into a function and name the function after the 'what'.", 'Martin Fowler'),
+(10, 'The real problem is that programmers have spent far too much time worrying about efficiency in the wrong places and at the wrong times; premature optimization is the root of all evil (or at least most of it) in programming.', 'Donald Knuth'),
+(11, 'SQL, Lisp, and Haskell are the only programming languages that I’ve seen where one spends more time thinking than typing.', 'Philip Greenspun'),
+(12, 'Deleted code is debugged code.', 'Jeff Sickel'),
+(13, 'There are two ways of constructing a software design: One way is to make it so simple that there are obviously no deficiencies and the other way is to make it so complicated that there are no obvious deficiencies.', 'C.A.R. Hoare'),
+(14, 'Simplicity is prerequisite for reliability.', 'Edsger W. Dijkstra'),
+(15, 'There are only two hard things in Computer Science: cache invalidation and naming things.', 'Phil Karlton'),
+(16, 'Measuring programming progress by lines of code is like measuring aircraft building progress by weight.', 'Bill Gates');
+
+SELECT * FROM quote;
+
+CREATE TABLE IF NOT EXISTS users (
+id int(11) NOT NULL,
+name varchar(200) NOT NULL,
+email varchar(200) NOT NULL,
+created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+ALTER TABLE users ADD PRIMARY KEY (id);
+ALTER TABLE users MODIFY id int(11) NOT NULL AUTO_INCREMENT;
+
+INSERT INTO users (id, name, email, created_at) VALUES
+  (1, 'Test', 'test@g.co', '2019-02-28 13:20:20'),
+  (2, 'john', 'john@g.co', '2019-02-28 13:20:20'),
+  (3, 'tutsmake', 'tuts@g.co', '2019-02-28 13:20:20'),
+  (4, 'tut', 'tut@g.co', '2019-02-28 13:20:20'),
+  (5, 'mhd', 'mhd@g.co', '2019-02-28 13:20:20');
+
 
 npm install mysql2 --save
 
 npm install cors --save
+
+
+git add .
+git commit -m "rest commit"
+git branch -M main
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var Express = require('express')();
-var users = require('./users');
+var usernames = require('./users');
 var bodyParser = require('body-parser');
-// var MySQL2 = require('mysql2');
+var MySQL2 = require('mysql2');
 // var CORS = require('cors');
 
 ///// port 8888 
@@ -14,12 +14,12 @@ Express.get('/', function (req, res) {
 Express.get('/index', function (req, res) {
     res.send('<h1>This is index page</h1>');
 });
-Express.get('/user', function (req, res) {
-    res.json(users.findAll());
+Express.get('/username', function (req, res) {
+    res.json(usernames.findAll());
 });
-Express.get('/user/:id', function (req, res) {
+Express.get('/username/:id', function (req, res) {
     var id = req.params.id;
-    res.json(users.findById(id));
+    res.json(usernames.findById(id));
 });
 
 // parse Expresslication/json
@@ -32,19 +32,150 @@ Express.post('/newuser', function (req, res) {
     res.send('Add new ' + json.name + ' Completed!');
 });
 
+var dbConn = MySQL2.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "mysql#pass",
+    port: 3306,
+    waitForConnections: true,
+    debug: false,
+    database: "todos"
+});
+dbConn.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
 
-// var con = MySQL2.createConnection({
-//     host: "localhost",
-//     user: "myuser",
-//     password: "test#pass"
-//     port: 3306
-//     database: "myDB"
-//   });
-//   con.connect(function(err) {
-//     if (err) throw err;
-//     console.log("Connected!");
-//   });
+// Retrieve all quote
+Express.get('/quotes', function (req, res) {
+    dbConn.query('SELECT * FROM quote', function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'quote list.' });
+    });
+});
+// Retrieve quote with id 
+Express.get('/quote/:id', function (req, res) {
+ 
+    let quote_id = req.params.id;
+ 
+    if (!quote_id) {
+        return res.status(400).send({ error: true, message: 'Please provide quote_id' });
+    }
+ 
+    dbConn.query('SELECT * FROM quote where id=?', quote_id, function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results[0], message: 'quote list.' });
+    });
+ 
+});
+// Add a new quote  
+Express.post('/quote', function (req, res) {
+ 
+    let quote = req.body.quote;
+ 
+    if (!quote) {
+        return res.status(400).send({ error:true, message: 'Please provide quote' });
+    }
+ 
+    dbConn.query("INSERT INTO quote SET ? ", { quote: quote }, function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'New user has been created successfully.' });
+    });
+});
+//  Update quote with id
+Express.put('/quote', function (req, res) {
+ 
+    let quote_id = req.body.quote_id;
+    let quote = req.body.quote;
+ 
+    if (!quote_id || !quote) {
+        return res.status(400).send({ error: quote, message: 'Please provide quote and quote_id' });
+    }
+ 
+    dbConn.query("UPDATE quote SET quote = ? WHERE id = ?", [quote, quote_id], function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'quote has been updated successfully.' });
+    });
+});
+//  Delete quote
+Express.delete('/quote', function (req, res) {
+ 
+    let quote_id = req.body.quote_id;
+ 
+    if (!quote_id) {
+        return res.status(400).send({ error: true, message: 'Please provide quote_id' });
+    }
+    dbConn.query('DELETE FROM quote WHERE id = ?', [user_id], function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'Quote has been updated successfully.' });
+    });
+}); 
 
+
+// Retrieve all users 
+Express.get('/users', function (req, res) {
+    dbConn.query('SELECT * FROM users', function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'users list.' });
+    });
+});
+// Retrieve user with id 
+Express.get('/user/:id', function (req, res) {
+ 
+    let user_id = req.params.id;
+ 
+    if (!user_id) {
+        return res.status(400).send({ error: true, message: 'Please provide user_id' });
+    }
+ 
+    dbConn.query('SELECT * FROM users where id=?', user_id, function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results[0], message: 'users list.' });
+    });
+ 
+});
+// Add a new user  
+Express.post('/user', function (req, res) {
+ 
+    let user = req.body.user;
+ 
+    if (!user) {
+        return res.status(400).send({ error:true, message: 'Please provide user' });
+    }
+ 
+    dbConn.query("INSERT INTO users SET ? ", { user: user }, function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'New user has been created successfully.' });
+    });
+});
+//  Update user with id
+Express.put('/user', function (req, res) {
+ 
+    let user_id = req.body.user_id;
+    let user = req.body.user;
+ 
+    if (!user_id || !user) {
+        return res.status(400).send({ error: user, message: 'Please provide user and user_id' });
+    }
+ 
+    dbConn.query("UPDATE users SET user = ? WHERE id = ?", [user, user_id], function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'user has been updated successfully.' });
+    });
+});
+//  Delete user
+Express.delete('/user', function (req, res) {
+ 
+    let user_id = req.body.user_id;
+ 
+    if (!user_id) {
+        return res.status(400).send({ error: true, message: 'Please provide user_id' });
+    }
+    dbConn.query('DELETE FROM users WHERE id = ?', [user_id], function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'User has been updated successfully.' });
+    });
+}); 
 // Express.use(cors())
 // Express.use(express.json())
 // Express.get('/usernames', function (req, res, next) {
